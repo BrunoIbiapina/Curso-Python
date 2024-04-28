bf0c028ab1cb9ffb2aaf4856febee586f8bef299
Bruno Ibiapina <brunoibiapina@me.com>
2024-04-27 23:12:33 -0300

    Finalizado

--- Exercicíos/calculadoracomwhile.py
+++ Exercicíos/calculadoracomwhile.py
@@ -5,28 +5,34 @@ while True:
     numero_2 = input('Digite outro número: ')
     operador = input('Digite o operador (+-/*): ')
 
-    numeros_validos = None
-
     try:
         num_1_float = float(numero_1)
         num_2_float = float(numero_2)
-        numeros_validos = True
-    except Exception:
-        numeros_validos = None
-
-    if numeros_validos is None:
-       print('Um dos números não são vádidos')
-       continue
-
-    operadores_permitidos = '+-/*'
-    if operador not in operadores_permitidos:
-        print('Um dos números não são vádidos')
+    except ValueError:
+        print('Um dos números não é válido.')
         continue
-    
-    if len(operador) > 1:
-        print('Digite apenas 1 operador')
+
+    if operador not in '+-*/':
+        print('Operador inválido.')
         continue
+
+    if operador == '/' and num_2_float == 0:
+        print('Não é possível dividir por zero.')
+        continue
+
+    print('Realizando sua conta')
+
+    if operador == '+':
+       print(num_1_float + num_2_float)
+    elif operador == '-':
+        print(num_1_float - num_2_float)
+    elif operador == '/':
+        print(num_1_float / num_2_float)
+    elif operador == '*':
+        print(num_1_float * num_2_float)
+    else:
+        print('Nunca deveria chegar aqui')
     
     sair = input('Quer sair? [s]im:').lower().startswith('s')
-    if sair is True:
-        break   
+    if sair:
+        break
